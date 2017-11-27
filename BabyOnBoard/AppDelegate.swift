1dfc07f9e1a93753016365ecbf6906c775553227
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2017-11-27 01:29:32 -0200

    Updated UI to mach the BabyOnBoard Standards

--- BabyOnBoard/AppDelegate.swift
+++ BabyOnBoard/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Override point for customization after application launch.
     let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
 
-    statusBar.backgroundColor = UIColor(red: 201/255, green: 214/255.0, blue: 253/255, alpha: 1.0)
+    statusBar.backgroundColor = UIColor(red: 201/237, green: 214/109, blue: 253/126, alpha: 1.0)
     return true
   }
 
